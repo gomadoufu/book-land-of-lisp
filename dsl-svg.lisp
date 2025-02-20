77dd4948c6930d6773ea6ec418dd39fd8aa984f5
gomadoufu <49393637+gomadoufu@users.noreply.github.com>
2025-02-20 19:34:41 +0900

    マクロ、頭を使います

---
+++ dsl-svg.lisp
@@ -0,0 +1,30 @@
+;;; svgを出力するDSLを作成する
+
+(defun print-tag (name alst closingp)
+  (princ #\<)
+  (when closingp
+    (princ #\/))
+  (princ (string-downcase name))
+  (mapc (lambda (att)
+          (format t " ~a=\"~a\"" (string-downcase (car att))(cdr att)))
+        alst)
+  (princ #\>))
+
+;; 補助関数pairs
+(defun pairs (lst)
+  (labels ((f (lst acc)
+             (split lst
+                    (if tail
+                        (f (cdr tail) (cons (cons head (car tail)) acc))
+                        (reverse acc))
+                    (reverse acc))))
+    (f lst nil)))
+
+(defmacro tag (name atts &body body)
+  `(progn (print-tag ',name
+                     (list ,@(mapcar (lambda (x)
+                                       `(cons ',(car x) ,(cdr x)))
+                                     (pairs atts)))
+                     nil)
+          ,@body
+          (print-tag ',name nil t)))
--- macro.lisp
+++ macro.lisp
@@ -47,3 +47,45 @@
                (tail (cdr x)))
            ,yes)
          ,no)))
+
+;; ついに安全なバージョンができた
+(defmacro split (val yes no)
+  (let1 g (gensym)
+    `(let1 ,g ,val
+       (if ,g
+           (let ((head (car ,g))
+                 (tail (cdr ,g)))
+             ,yes)
+           ,no))))
+
+(macroexpand '(split '(2 3)
+               (+ x head)
+               nil))
+
+;; my-lengthのfも省略したい
+;; 補助関数pairs
+(defun pairs (lst)
+  (labels ((f (lst acc)
+             (split lst
+                    (if tail
+                        (f (cdr tail) (cons (cons head (car tail)) acc))
+                        (reverse acc))
+                    (reverse acc))))
+    (f lst nil)))
+
+;; recurse
+(defmacro recurse (vars &body body)
+  (let1 p (pairs vars)
+    `(labels ((self ,(mapcar #'car p)
+                ,@body))
+       (self ,@(mapcar #'cdr p)))
+    )
+  )
+
+(defun my-length (lst)
+  (recurse (lst lst
+                acc 0)
+           (split lst
+                  (self tail (1+ acc))
+                  acc)))
+
